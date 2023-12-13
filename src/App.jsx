3220a0462592a4db8bf38e9350c892acd88746a2
Maverick017 <anirsafid17@gmail.com>
2023-12-13 12:25:15 +0600

    Safid1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState,useEffect} from 'react'
+import {fetchDataFromApi} from "./utils/api"
+
+function App() {
+  useEffect(() => {
+    apitest();
+  },[])
+  const apitest = () =>{
+    fetchDataFromApi('/movie/popular')
+    .then((res) => {
+      console.log(res)
+    })
+  }
+  return <div className='App'>App</div>
+}
+export default App
